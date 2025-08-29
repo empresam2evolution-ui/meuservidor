6ce814776f2e1d470d0ba610dfd37133362bcc4e
empresam2evolution-ui <empresam2evolution@gmail.com>
2025-08-29 15:04:52 -0300

    primeiro deploy

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from app import app, socketio
 
+# Gunicorn procura esse objeto "app"
+application = app  
+
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, host="0.0.0.0", port=5000)
